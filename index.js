3ab3d6411afe8155a2d22a9da1a40c065eb14257
Sham Kishor Sharma <shamsharm@Shams-MacBook-Air.local>
2019-01-13 15:32:07 +0530

    Our first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+//----- Usign Underscore npm Module ----------
+const _ = require('underscore');
+
+const results = _.contains([1,2,3], 3);
+console.log(results);
+
+//------------XXXX-----------
